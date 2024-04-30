cadd40b7ad0826b9ad99d1c88d6a28b2fd842edd
Saks George <georgesaks03@gmail.com>
2024-04-30 20:15:27 +0000

    input_variables_datatype_num

---
+++ data_type.py
@@ -0,0 +1,7 @@
+#greeting = 'Hello, friend'
+#age = 35  Interger
+#age = '35' string
+#speed = 4.5 (Float)
+#speed = 4.6 (Float)FloatingPointError
+#speed = 4  (interger)
+#am_I_ugly = True or False this is Boolean and answer is True or false in capital letter
---
+++ operators.py
@@ -0,0 +1,29 @@
+print(2 + 3)
+print(5-4)
+print(3 * 5)
+print(6 / 2)
+print(7/2)
+print(7//2)
+#modulous
+print(6//2)
+print(5.5 + 2 )
+print(2.2 + 1.4)
+
+age = 28
+print(age)
+
+age = 28
+new_age = age + 5
+print(new_age)
+
+age = 28
+age = age + 7
+print(age)
+
+
+age = 28
+age += 7
+print(age)
+
+
+
