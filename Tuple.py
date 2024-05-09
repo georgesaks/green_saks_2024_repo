2b6875e3356551d7d7532d28df36aad27713cf6c
Saks George <georgesaks03@gmail.com>
2024-05-09 03:23:15 +0000

    loop&dictionary

---
+++ Tuple.py
@@ -0,0 +1,47 @@
+
+one_el_tuple_a = (1)
+print(one_el_tuple_a)
+
+user_data = ('John', 'American' '1964')
+print(user_data[0])
+
+#message[0] = w
+
+
+#Tuple Operation
+
+user_data = ('John', 'American', '1964')
+print(len(user_data))
+
+
+user_data = ('John', 'American', '1964')
+if 'American' in user_data:
+    print('This person is an American')
+    
+    
+user_data = ('John', 'American', '1964')
+for element in user_data:
+    print(element)
+    
+    
+    
+    
+#ADDITION
+user_data = ('John', 'American', '1964') + ('teacher', 'male')
+print(user_data)
+
+numbers = (0,1) * 10
+print(numbers)
+
+city_1 = ('london', 'uk', 4.5)
+city_2 = ('Abuja', 'Nigeria', 103)
+city_3 = ('Abijan', 'Senegal', 3)
+capitals = [('london', 'uk', 4.5),('Abuja', 'Nigeria', 103), ('Abijan', 'Senegal', 3)]
+
+
+for capital in capitals:
+    print('name:', capital[0], ', country:', capital[1], ', Population:', capital[2])
+
+    
+    
+    
